e793a1630d0f8882b839eb3209186746c6e000d6
salvatoto <salvanakamoto@protonmail.com>
2023-06-10 12:00:53 -0400

    Add days 2-4 + goals page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { allPosts } from "@/.contentlayer/generated"
+import { compareAsc, compareDesc, format, parseISO } from 'date-fns'
 import Link from "next/link"
 
 export default function Home() {
+  const posts = allPosts.sort((a, b) => compareAsc(new Date(a.date), new Date(b.date)))
+
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {posts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
